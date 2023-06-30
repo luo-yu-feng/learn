71bb9892b4e7281d52bf9326b34c2d65d43537e9
liuyunfei <liuyunfei@exiot.cmcc>
2023-06-30 14:18:26 +0800

    项目创建

---
+++ src/test/java/com/lyf/demo/DemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.lyf.demo;
+
+import java.util.Collection;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+
+  }
+
+}
